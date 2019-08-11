9af23dd606a5e9f23ab4e81855e84927b77c9827
Jan Cavel <caveljan@gmail.com>
2019-08-11 17:21:23 +0300

    state-recorder > test: StateRecorder, reset(), branch()

--- packages/state-recorder/src/objects/StateRecorder/test.spec.ts
+++ packages/state-recorder/src/objects/StateRecorder/test.spec.ts
@@ -103,4 +103,66 @@ describe('StateRecorder', () => {
         const states1 = stateRecorder.all();
         expect(states1).toStrictEqual([{}]);
     });
+
+    it('can be resetted', () => {
+        const state1 = {
+        };
+        const stateRecorder = new StateRecorder(state1);
+        const states1 = stateRecorder.all();
+        expect(states1).toStrictEqual([state1]);
+
+        const state2 = {
+            action1: 'foo',
+        };
+        stateRecorder.add(state2);
+
+        const states2 = stateRecorder.all();
+        expect(states2).toStrictEqual([state1, state2]);
+
+        const state3 = {
+            action1: 'foo',
+            action2: 'boo',
+        };
+        stateRecorder.add(state3);
+
+        stateRecorder.reset();
+        const statesAfterReset = stateRecorder.all();
+        expect(statesAfterReset).toStrictEqual([{}]);
+    });
+
+    it('can be branched', () => {
+        const state1 = {
+        };
+        const stateRecorder = new StateRecorder(state1);
+        const states1 = stateRecorder.all();
+        expect(states1).toStrictEqual([state1]);
+
+        const state2 = {
+            action1: 'foo',
+        };
+        stateRecorder.add(state2);
+
+        const states2 = stateRecorder.all();
+        expect(states2).toStrictEqual([state1, state2]);
+
+        const state3 = {
+            action1: 'foo',
+            action2: 'boo',
+        };
+        stateRecorder.add(state3);
+
+        // go to state2
+        const previousStateSituation = stateRecorder.previous();
+        expect(previousStateSituation.state).toStrictEqual(state2);
+
+        const statesAllBeforeBranch = stateRecorder.all();
+        expect(statesAllBeforeBranch).toStrictEqual([state1, state2, state3]);
+        expect(statesAllBeforeBranch.length).toBe(3);
+
+        stateRecorder.branch();
+
+        const statesAllAfterBranch = stateRecorder.all();
+        expect(statesAllAfterBranch.length).toBe(2);
+        expect(statesAllAfterBranch).toStrictEqual([state1, state2]);
+    });
 });
